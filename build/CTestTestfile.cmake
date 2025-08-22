02ed0a6748086d1b19caea418891923be5515f4e
Louise Bilous <louise.bilous@siemens.com>
2025-08-22 10:32:33 +0100

    Remove ignored files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Practise/cpp_chess
-# Build directory: C:/Practise/cpp_chess/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
