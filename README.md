a0bd7582e17de0a25f7a2d41b61bb175b059b306
Louise Bilous <louise.bilous@siemens.com>
2025-08-22 10:24:23 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# SFML Chess C++
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Practise/cpp_chess
+# Build directory: C:/Practise/cpp_chess/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
