7b950229cb3ed14c0e1384089338467699d9476a
Louise Bilous <bilouslouise@gmail.com>
2025-08-26 20:44:33 +0100

    Working on my laptop at home...

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,21 @@ using namespace std;
 
 int main() {
     cout << "Hello World!" << endl;
+
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
+ 
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        while (const std::optional event = window.pollEvent())
+        {
+            // Close window: exit
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+    }
+
     return 0;
 }
